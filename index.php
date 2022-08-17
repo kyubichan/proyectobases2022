bcb68d3445a8d7d97b012b88908379e174def6ba
kyubichan <110320722+kyubichan@users.noreply.github.com>
2022-08-17 16:17:43 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 
+
+    
+    
+    
+</body>
+</html>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,18 +13,66 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="css/estilos.css">
+    <link rel="stylesheet" href="css/login.css">
 </head>
 <body>
-	<center>
-	<h1 class="miclase1"><i class="fad fa-user"></i>Login de usuario</h1>
-	<form action="sesion.php" method="POST">
-		<input type="text" name="user" placeholder="Ingrese su usuario">
-		<br><br>
-		<input type="password" name="pass" placeholder="Ingrese su contraseña">
-		<br><br>
-		<button type="submit" >Ingresar</button>
-	</form>
-	</center>
+
+
+	<table class="login">
+
+	<tr><th style="height:30px;"></th></tr>
+	
+
+	<tr> 
+
+		
+			<th style="height:150px;">
+				<h1 class="miclase1">
+					<i class="fad fa-user"></i>BIENVENIDO
+				</h1>	
+			</th>
+	
+		
+	</tr>
+
+	<tr>
+
+		<td style="height:auto">
+
+			<div class="contenedor">
+
+		
+
+			<form action="sesion.php" method="POST">
+
+				<input class="usuario_boton" type="text" name="user" placeholder="Ingrese su usuario">
+
+				<br><br>
+
+				<input class="usuario_boton" type="password" name="pass" placeholder="Ingrese su contraseña">
+
+				<br>
+
+				<div class="ingresar">
+
+					<button class="ingresar_boton" type="submit" >Ingresar</button>
+
+				</div>
+
+				
+
+				
+
+			</form>
+		
+
+			</div>
+		</td>
+		
+	</tr>
+
+	</table>
+	
+
 </body>
 </html>
